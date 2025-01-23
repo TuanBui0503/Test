b81dd8f44084d634ace700415c87d27dc52a560f
TuanBui0503 <buiquoctuan532001@gmail.com>
2025-01-23 22:23:00 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,15 @@
+// các kiểu dữ liệu trong JS
+// - Number
+// - Boolean kiểm tra đúng 
+// - undefined báo biến nhưng chưa gán gì
+// - null không có gì cả
+// - Symbol
+// - function hàm
+// - ofjiect là đối tượng có thể chứa nhiều loại dữ liệu
+
+
+// Mảng
+const mang = ['trau' , 'cho', 'lon']
+// console.log(mang[1]);
+// thêm phần tử vào mảng
+// console.log(mang.join('vit'));
